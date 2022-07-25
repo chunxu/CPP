b75a6a9f5f4922ef7e941c5a620ef82cd02bb137
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-24 20:37:18 -0500

    Create Pyramid.cpp

---
+++ Pyramid.cpp
@@ -0,0 +1,77 @@
+# Write a C++ program that displays a Letter Pyramid from a user-provided std::string.
+
+# Prompt the user to enter a std::string and then from that string display a Letter Pyramid as follows:
+
+# It's much easier to understand the Letter Pyramid given examples.
+
+# If the user enters the string "ABC", then your program should display:
+
+#   A                                                                                                                    
+#  ABA                                                                                                                   
+# ABCBA 
+
+
+# If the user enters the string, "12345", then your program should display:
+
+ 
+
+#     1                                                                                                                  
+#    121                                                                                                                 
+#   12321                                                                                                                
+#  1234321                                                                                                               
+# 123454321
+ 
+
+# If the user enters 'ABCDEFG', then your program should display:
+
+ 
+
+#       A                                                                                                                
+#      ABA                                                                                                               
+#     ABCBA                                                                                                              
+#    ABCDCBA                                                                                                             
+#   ABCDEDCBA                                                                                                            
+#  ABCDEFEDCBA                                                                                                           
+# ABCDEFGFEDCBA
+# If the user enters 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', then your program should display:
+
+#                          A
+#                         ABA
+#                        ABCBA
+#                       ABCDCBA
+#                      ABCDEDCBA
+#                     ABCDEFEDCBA
+#                    ABCDEFGFEDCBA
+#                   ABCDEFGHGFEDCBA
+#                  ABCDEFGHIHGFEDCBA
+#                 ABCDEFGHIJIHGFEDCBA
+#                ABCDEFGHIJKJIHGFEDCBA
+#               ABCDEFGHIJKLKJIHGFEDCBA
+#              ABCDEFGHIJKLMLKJIHGFEDCBA
+#             ABCDEFGHIJKLMNMLKJIHGFEDCBA
+#            ABCDEFGHIJKLMNONMLKJIHGFEDCBA
+#           ABCDEFGHIJKLMNOPONMLKJIHGFEDCBA
+#          ABCDEFGHIJKLMNOPQPONMLKJIHGFEDCBA
+#         ABCDEFGHIJKLMNOPQRQPONMLKJIHGFEDCBA
+#        ABCDEFGHIJKLMNOPQRSRQPONMLKJIHGFEDCBA
+#       ABCDEFGHIJKLMNOPQRSTSRQPONMLKJIHGFEDCBA
+#      ABCDEFGHIJKLMNOPQRSTUTSRQPONMLKJIHGFEDCBA
+#     ABCDEFGHIJKLMNOPQRSTUVUTSRQPONMLKJIHGFEDCBA
+#    ABCDEFGHIJKLMNOPQRSTUVWVUTSRQPONMLKJIHGFEDCBA
+#   ABCDEFGHIJKLMNOPQRSTUVWXWVUTSRQPONMLKJIHGFEDCBA
+#  ABCDEFGHIJKLMNOPQRSTUVWXYXWVUTSRQPONMLKJIHGFEDCBA
+# ABCDEFGHIJKLMNOPQRSTUVWXYZYXWVUTSRQPONMLKJIHGFEDCBA
+# If the user enters "C++isFun!", then your program should display:
+
+ 
+
+#         C                                                                                                              
+#        C+C                                                                                                             
+#       C+++C                                                                                                            
+#      C++i++C                                                                                                           
+#     C++isi++C                                                                                                          
+#    C++isFsi++C                                                                                                         
+#   C++isFuFsi++C                                                                                                        
+#  C++isFunuFsi++C                                                                                                       
+# C++isFun!nuFsi++C
+
